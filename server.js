c834ac765cca72017ba1b10efda38d97a9f9350f
ansh-2609 <anshku037@gmail.com>
2026-01-03 14:53:51 +0530

    first-commit

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "contact-management-backend-sigma.vercel.app",
+    "https://contact-management-backend-sigma.vercel.app",
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
 }));
